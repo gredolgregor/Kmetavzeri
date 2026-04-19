a25d66c3123da5cb6aab4554f51959c13783d237
theodora3333 <teodora.stojanovska@student.um.si>
2026-04-19 20:25:41 +0200

    Update index.php

--- data/www/index.php
+++ data/www/index.php
@@ -1,1 +1,23 @@
-<?php echo "Deluje!"; ?>
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kulinarika</title>
+</head>
+<body>
+<h1>Kulinarika</h1>
+<p>
+    Na naši kmetiji vas razvajamo z domačimi jedmi, pripravljenimi iz ekoloških sestavin 
+    po tradicionalnih receptih. Oživljamo okuse Pohorja in skoraj pozabljene jedi, 
+    kjer se prepletajo narava, tradicija in pristna domačnost.
+</p>
+<img src="image/kulinarikabro.png"> 
+<hr/>
+<p>
+    Pri nas kulinarika ni le spremljevalni del bivanja, temveč pomemben del doživetja kmetije,
+     kjer vsak grižljaj pripoveduje zgodbo o domačnosti, kakovosti in spoštovanju do tradicije.
+</p>
+<img src="">
+</body>
+</html>
